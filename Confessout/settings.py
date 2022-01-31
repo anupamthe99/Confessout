1e85ae1e5d98586ee6fe30097de4450eac7b2363
Anupam <puspaanupam@gmail.com>
2022-01-31 14:51:17 +0545

    Update settings.py

--- Confessout/settings.py
+++ Confessout/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c+jzhn=4pq-iczuqk629xtv%3kt1#u+^8tmxdg&3x2aftcl8$v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','hamroconfession.herokuapp.com']
 
 
 # Application definition
